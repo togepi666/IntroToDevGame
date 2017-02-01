4cb3811bc45d36e31d6075a1ffafb0c1111b099f
Unknown <egr274@nyu.edu>
2017-02-01 16:23:47 -0500

    Put Unity project into Git Repository

---
+++ Assets/GameSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSystem : MonoBehaviour {
+    public GameObject squares;
+    public int points;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnSquares", 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void SpawnSquares()
+    {
+        Instantiate(squares, new Vector3(Random.Range(-9,10), Random.Range(-3,4), 0), Quaternion.identity);
+    }
+}
---
+++ Assets/Identity.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Identity : MonoBehaviour {
+    Color[] listOfColors = new Color[9];
+    string[] codes = new string[9];
+    string identity;
+	// Use this for initialization
+	void Start () {
+        listOfColors[0] = new Color(.2f,.1f,0f);
+        listOfColors[1] = new Color(1f, .3f, .23f);
+        listOfColors[2] = new Color(.12f, .22f, .103f);
+        listOfColors[3] = new Color(.11f, .55f, .9f);
+        listOfColors[4] = new Color(.65f, .93f, .33f);
+        listOfColors[5] = new Color(.42f, 1f, .23f);
+        listOfColors[6] = new Color(1f, .43f, 1f);
+        listOfColors[7] = new Color(.5f, .8f, .9f);
+        listOfColors[8] = new Color(.3f, .4f, .7f);
+        codes[0] = "1";
+        codes[1] = "2";
+        codes[2] = "3";
+        codes[3] = "4";
+        codes[4] = "5";
+        codes[5] = "6";
+        codes[6] = "7";
+        codes[7] = "8";
+        codes[8] = "9";
+        int random = (int)Random.Range(0,9);
+        GetComponent<SpriteRenderer>().color = listOfColors[random];
+        identity = codes[random];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.inputString == identity)
+        {
+            Destroy(gameObject);
+            //points++;
+        }
+	}
+}
