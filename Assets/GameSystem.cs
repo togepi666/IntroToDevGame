d46d5bbd3f4be55b3e31ecc29e961de5e8eeaea2
Unknown <egr274@nyu.edu>
2017-02-06 14:52:43 -0500

    Update unity project

--- Assets/GameSystem.cs
+++ Assets/GameSystem.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class GameSystem : MonoBehaviour {
     public GameObject squares;
     public int points;
+    public Text pointsText;
+    public int playerHP = 7;
+    public int pressingAbility = 5;
+    bool gameIsPlaying = true;
+    public bool canPlayerPressingButtons = true;
+
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("SpawnSquares", 1, 1);
@@ -11,11 +17,36 @@ public class GameSystem : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
-	}
+        pointsText.text = "Points:" + points;
+        playerLife();
+        Debug.Log(gameIsPlaying);
+        Debug.Log(pressingAbility);
+    }
 
     void SpawnSquares()
     {
         Instantiate(squares, new Vector3(Random.Range(-9,10), Random.Range(-3,4), 0), Quaternion.identity);
+    }
+    public void increasePoint(int worth)
+    {
+        points = points + worth;
+    }
+    void playerLife()
+    {
+        if(playerHP <= 0)
+        {
+            gameIsPlaying = false;
+        }
+    }
+    void checkPlayerPressingButtons()
+    {
+        if (pressingAbility <= 0)
+        {
+            canPlayerPressButtons = false;
+        }
+    }
+    void increasePressingButtons()
+    {
+        pressingAbility++;
     }
 }
--- Assets/Identity.cs
+++ Assets/Identity.cs
@@ -4,8 +4,21 @@ public class Identity : MonoBehaviour {
     Color[] listOfColors = new Color[9];
     string[] codes = new string[9];
     string identity;
+    int lifeSpan;
+    public GameSystem gs;
+    int uniqueState;
+    bool isUnique = false;
+    int worth = 1;
 	// Use this for initialization
 	void Start () {
+        uniqueState = (int)Random.Range(0,10);
+        if(uniqueState == 2)
+        {
+            isUnique = true;
+            worth = 5;
+        }
+        lifeSpan = (int)Random.Range(2, 10);
+        gs = FindObjectOfType(typeof(GameSystem)) as GameSystem;
         listOfColors[0] = new Color(.2f,.1f,0f);
         listOfColors[1] = new Color(1f, .3f, .23f);
         listOfColors[2] = new Color(.12f, .22f, .103f);
@@ -27,14 +40,30 @@ public class Identity : MonoBehaviour {
         int random = (int)Random.Range(0,9);
         GetComponent<SpriteRenderer>().color = listOfColors[random];
         identity = codes[random];
+        InvokeRepeating("SquareLife", 0,1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.inputString == identity)
         {
+            gs.pressingAbility = gs.pressingAbility - 1;
             Destroy(gameObject);
-            //points++;
+            gs.increasePoint(worth);
         }
+        checkSquareLife();
 	}
+    void SquareLife()
+    {
+        lifeSpan--;
+    }
+    void checkSquareLife()
+    {
+        if (lifeSpan == 0)
+        {
+            Destroy(gameObject);
+            gs.playerHP--;
+        }
+    }
+
 }
